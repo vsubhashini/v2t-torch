e8b3d22c1beb1422886537af662b7af8828e7944
Shaayaan Sayed <ssayed@cs.utexas.edu>
2016-01-09 16:20:52 -0600

    completed base for meanpool and frames, includes val loss evaluation

--- train.lua
+++ train.lua
@@ -16,6 +16,7 @@ cmd:text('Options')
 cmd:option('-model', 'meanpool', 'type of model to use? meanpool|frames|frames_cnn')
 cmd:option('-num_layers', 2, 'number of layers in lstm')
 cmd:option('-rnn_size', 512, 'size of the rnn in number of hidden nodes in each layer')
+cmd:option('-drop_prob', 0, 'probability for dropout (0 = no dropout)')
 -- data loading/pre-processing
 cmd:option('-video_dir', '', 'directory where to read video data from')
 cmd:option('-label_dir', '', 'directory where to read labels/captions from')
@@ -31,10 +32,16 @@ cmd:option('-learning_rate', 4e-4,'learning rate')
 cmd:option('-optim_alpha',0.8,'alpha for adagrad/rmsprop/momentum/adam')
 cmd:option('-optim_beta',0.999,'beta used for adam')
 cmd:option('-optim_epsilon',1e-8,'epsilon that goes into denominator for smoothing')
+cmd:option('-decay_start', -1, 'at what iteration to start decaying learning rate? (-1 = dont)')
+cmd:option('-decay_rate', 50000, 'decay rate')
 -- printing updates and saving checkpoints
+cmd:option('-lang_metric','METEOR','metric to use for saving checkpoints METEOR|CIDEr|ROUGE_L')
 cmd:option('-print_every',1,'how many steps/minibatches between printing out the loss')
+cmd:option('-eval_model_every', 200, 'how many steps/minibatches between loss and language evaluation on model')
+cmd:option('-eval_val_loss', 0, 'evaluate and save validation loss during checkpoints (1 == yes, 0 == no)')
+cmd:option('-save_model_dir', 'cv/','directory to save checkpoints and score/loss evaluations')
+cmd:option('-save_model_name', 'frames_1','name of model')
 -- misc
-cmd:option('-id', 1, 'an id identifying this run/job. used when writing progress files')
 cmd:option('-seed', 123, 'random number generator seed to use')
 cmd:option('-gpuid', -1, 'which gpu to use. -1 = use CPU')
 local opt = cmd:parse(arg)
@@ -83,6 +90,53 @@ protos.lm:createClones()
 
 collectgarbage()
 
+local function sampleSplit(split_ix)
+  protos.lm:evaluate()
+  loader:resetIterator(split_ix) 
+
+  local numSamples = loader:splitSize(split_ix)
+  local splitSamples = {}
+
+  local vidIds = {}
+  for ix=1,numSamples do
+	  local video, _, id = loader:getBatch(split_ix)
+
+	  if opt.gpuid >= 0 then video, _ = net_utils.send_to_gpu(video) end
+
+    local sample, logprobs = protos.lm:sample(video, {sample_max=opt.sample_max, temperature=opt.temperature})
+
+    table.insert(splitSamples, sample)
+    table.insert(vidIds, id)
+  end
+
+  return splitSamples, vidIds
+end
+
+local function evalSplit(split_ix)
+  protos.lm:evaluate()
+  loader:resetIterator(split_ix) 
+
+  local totalLoss = 0
+  local numEvals = loader:splitSize(split_ix)
+
+  for i=1,numEvals do
+
+    -- fetch a batch of data
+ 		local batchVideos, batchLabels, _ = loader:getBatch(split_ix)
+
+		if opt.gpuid >= 0 then batchVideos, batchLabels = net_utils.send_to_gpu(batchVideos, batchLabels) end
+
+    -- forward the model to get loss
+    local logprobs = protos.lm:forward{batchVideos, batchLabels}
+    local loss = protos.crit:forward(logprobs, batchLabels)
+    totalLoss = totalLoss + loss
+
+    print(i .. '/' .. numEvals .. '... ' .. loss)
+  end
+
+  return totalLoss/numEvals
+end
+
 local function lossFun()
   protos.lm:training()
   grad_params:zero()
@@ -130,11 +184,11 @@ while true do
 
   -- decay learning rate 
   local learning_rate = opt.learning_rate
-  -- if epoch > opt.learning_rate_decay_start and opt.learning_rate_decay_start >= 0 then
-  --   local epochs_over_start = math.ceil(epoch - opt.learning_rate_decay_start)
-  --   local decay_factor = math.pow(opt.decay_rate, epochs_over_start)
-  --   learning_rate = learning_rate * decay_factor -- set the decayed rate
-  -- end
+  if epoch > opt.decay_start and opt.decay_start >= 0 then
+    local epochs_over_start = math.ceil(epoch - opt.decay_start)
+    local decay_factor = math.pow(opt.decay_rate, epochs_over_start)
+    learning_rate = learning_rate * decay_factor -- set the decayed rate
+  end
 
   update:zero()
   -- optimization step
@@ -155,30 +209,33 @@ while true do
   end
 
   -- save checkpoint based on language evaluation
-  -- if (iter % opt.save_checkpoint_every == 0 or (epoch >= opt.epochs and opt.epochs > 0)) then
+  if (iter % opt.eval_model_every == 0 or (epoch >= opt.epochs and opt.epochs > 0)) then
 
-  --   local splitSamples, ids = sample_split(3)
-  --   score, samples = utils.lang_eval(splitSamples, ids, 'METEOR')
+  	local loss
+  	if opt.eval_val_loss > 0 then loss = evalSplit(2) end
+    local splitSamples, ids = sampleSplit(3)
+    scores, samples = utils.lang_eval(splitSamples, ids)
 
-  --   -- save the model if it performs better than ever
-  --   if score > best_score then
-  --     local checkpoint_path = path.join(opt.checkpoint_dir, 'id_' .. opt.id)
+    -- save the model if it performs better than ever
+    if scores[opt.lang_metric] > best_score then
+      local checkpoint_path = path.join(opt.save_model_dir, opt.save_model_name)
 
-  --     local checkpoint_info = {}
-  --     checkpoint_info.opt = opt
-  --     checkpoint_info.epoch = epoch
-  --     checkpoint_info.vocab = ix_to_word
-  --     checkpoint_info.score = score
-  --     checkpoint_info.samples = samples
-  --     utils.write_json(checkpoint_path .. '.json', checkpoint_info)
+      local checkpoint_info = {}
+      checkpoint_info.opt = opt
+      checkpoint_info.epoch = epoch
+      checkpoint_info.vocab = ix_to_word
+      checkpoint_info.scores = scores
+      checkpoint_info.samples = samples
+      if opt.eval_val_loss > 0 then checkpoint_info.loss = loss end
+      utils.write_json(checkpoint_path .. '.json', checkpoint_info)
 
-  --     local save_protos = {}
-  --     save_protos.lm = thin_lm
-  --     torch.save(checkpoint_path .. '.t7', save_protos)
+      local save_protos = {}
+      save_protos.lm = thin_lm
+      torch.save(checkpoint_path .. '.t7', save_protos)
 
-  --     best_score = score
-  --   end
-  -- end
+      best_score = scores[opt.lang_metric]
+    end
+  end
 
   if iter % opt.print_every == 0 then
     print(string.format("%d (epoch %.3f), train_loss = %6.8f, ratio = %6.4e", iter, epoch, losses.total_loss, update:norm()/params:norm()))
